6aaa6775b856784e5f2f7664c6ca46d36dfe3d29
Emily <emilyjackson2018@yahoo.com>
2018-02-13 20:57:45 -0500

    Welcome Page
    2 mockups of the Intro/Welcome Page

---
+++ Stumble/Stumble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stumble
+//
+//  Created by Emily Jackson on 2/13/18.
+//  Copyright © 2018 Emily Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
